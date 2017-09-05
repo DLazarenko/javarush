89160b9fd37fc3332bf233fadb3504ed1d536a74
DLazarenko <daria.lazarenko20@gmail.com>
2017-09-05 23:14:04 +0300

    Snake

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task23.task2305;
+
+/* 
+Реализовать метод getTwoSolutions, который должен возвращать массив из 2-х экземпляров класса Solution.
+Для каждого экземпляра класса Solution инициализировать поле innerClasses двумя значениями.
+Инициализация всех данных должна происходить только в методе getTwoSolutions.
+
+
+Требования:
+1. В классе Solution должен быть реализован метод getTwoSolutions.
+2. Метод getTwoSolutions должен быть статическим.
+3. Метод getTwoSolutions должен быть публичным.
+4. Метод getTwoSolutions должен возвращать массив типа Solution заполненный согласно заданию.
+*/
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+        Solution[] solutions = new Solution[2];
+        Solution solutionFirst = new Solution();
+        solutionFirst.innerClasses[0] = solutionFirst.new InnerClass();
+        solutionFirst.innerClasses[1] = solutionFirst.new InnerClass();
+        Solution solutionSecond = new Solution();
+        solutionSecond.innerClasses[0] = solutionSecond.new InnerClass();
+        solutionSecond.innerClasses[1] = solutionSecond.new InnerClass();
+        solutions[0] = solutionFirst;
+        solutions[1] = solutionSecond;
+        return solutions;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
