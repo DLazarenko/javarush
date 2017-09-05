48a40b7e16087a1c1db614ec0c988d57d1a36788
DLazarenko <daria.lazarenko20@gmail.com>
2017-09-05 23:15:51 +0300

    Inner classes

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -1,17 +1,10 @@
 package com.javarush.task.task23.task2305;
 
-/* 
-Реализовать метод getTwoSolutions, который должен возвращать массив из 2-х экземпляров класса Solution.
-Для каждого экземпляра класса Solution инициализировать поле innerClasses двумя значениями.
-Инициализация всех данных должна происходить только в методе getTwoSolutions.
-
-
-Требования:
-1. В классе Solution должен быть реализован метод getTwoSolutions.
-2. Метод getTwoSolutions должен быть статическим.
-3. Метод getTwoSolutions должен быть публичным.
-4. Метод getTwoSolutions должен возвращать массив типа Solution заполненный согласно заданию.
-*/
+/**
+ * Реализовать метод getTwoSolutions, который должен возвращать массив из 2-х экземпляров класса Solution.
+ * Для каждого экземпляра класса Solution инициализировать поле innerClasses двумя значениями.
+ * Инициализация всех данных должна происходить только в методе getTwoSolutions.
+ */
 public class Solution {
     public InnerClass[] innerClasses = new InnerClass[2];
 
@@ -32,6 +25,5 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-
     }
 }
