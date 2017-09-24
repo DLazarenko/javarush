bf93773cb90f2b9e8f3b79ac6034efaa0c61dda6
DLazarenko <daria.lazarenko20@gmail.com>
2017-09-24 22:55:19 +0300

    %3$S %4$s %2$s %1$s

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2205/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task22.task2205;
+
+/* 
+z
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(String.format(getFormattedString(), "JavaRush", "курс", "мне", "нравится"));
+        //должен быть вывод
+        //"МНЕ нравится курс JavaRush"
+    }
+    public static String getFormattedString() {
+        return "%3$S %4$s %2$s %1$s";
+    }
+}
